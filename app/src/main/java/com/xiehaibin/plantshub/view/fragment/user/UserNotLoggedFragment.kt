48879619a813b15d7362159d7c595af62d9bb992
xiehaibin18@gmail.com <xiehaibin18@gmail.com>
2020-02-28 20:27:28 +0800

    UserNotLogged finished

--- app/src/main/java/com/xiehaibin/plantshub/view/fragment/user/UserNotLoggedFragment.kt
+++ app/src/main/java/com/xiehaibin/plantshub/view/fragment/user/UserNotLoggedFragment.kt
@@ -6,9 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
 
 import com.xiehaibin.plantshub.R
+import com.xiehaibin.plantshub.view.activity.LoginActivity
 import com.xiehaibin.plantshub.viewModel.user.UserNotLoggedViewModel
+import kotlinx.android.synthetic.main.user_not_logged_fragment.*
+import org.jetbrains.anko.support.v4.startActivity
 
 class UserNotLoggedFragment : Fragment() {
 
@@ -28,7 +32,12 @@ class UserNotLoggedFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-
+        user_not_logged_login_button.setOnClickListener {
+            startActivity<LoginActivity>()
+        }
+        user_not_logged_back_button.setOnClickListener {
+            it.findNavController().navigate(R.id.action_userNotLoggedFragment_to_homeFragment)
+        }
     }
 
 }
